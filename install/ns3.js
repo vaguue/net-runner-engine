577601c2a8f965d5a90cb9a8173cd4a0f857c3e8
Valentina K <sabrinarempel7@gmail.com>
2022-11-29 05:50:28 +0300

    docker install

--- install/ns3.js
+++ install/ns3.js
@@ -12,7 +12,7 @@ const { systemsSupported, sharedObjects, include } = require('./initData');
 const rootDir = path.resolve(__dirname, '..');
 
 const config = {
-  enableGlobalSearch: false,
+  enableGlobalSearch: process.argv.includes('--enable-global-search'),
 };
 
 
